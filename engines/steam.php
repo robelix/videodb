7ad2f8c50db081e003e2a7f45d50de6439f68643
NF13 <6102845+NF13@users.noreply.github.com>
2019-09-09 22:28:50 +0200

    update steam engine

--- engines/steam.php
+++ engines/steam.php
@@ -30,18 +30,18 @@ function steamSearch($title, $aka=null)
     $data['encoding'] = $resp['encoding'];
 
     // multiple matches
-    if (preg_match_all('/"appid":\d+,"name":"[^"]*'.$title.'[^"]*"/i', $resp['data'], $multi, PREG_SET_ORDER))
+    if (preg_match_all('/"appid":\d+,"name":"[^"]*'.str_replace (' ', '[^"]*', $title).'[^"]*"/i', $resp['data'], $multi, PREG_SET_ORDER))
     {
         foreach ($multi as $row)
         {
-            preg_match('/"appid":(\d+),"name":"([^"]*'.$title.'[^"]*)"/i', $row[0], $ary);
+            preg_match('/"appid":(\d+),"name":"([^"]*'.str_replace (' ', '[^"]*', $title).'[^"]*)"/i', $row[0], $ary);
             if ($ary[1] and $ary[2]) {
                 $info           = array();
                 $info['id']     = $steamIdPrefix.$ary[1];
                 $info['title']  = $ary[2];
                 $data[]         = $info;
             }
-
+			$data = multidimsort($data, 'title');
 #           dump($info);
         }
     }
@@ -49,6 +49,19 @@ function steamSearch($title, $aka=null)
     return $data;
 }
 
+function multidimsort($array_in, $column)
+{
+    $multiarray = array_column($array_in, $column);
+    $array_out  = array();
+    asort($multiarray);
+    // traverse new array of index values and add the corresponding element of the input array to the correct position in the output array
+    foreach ($multiarray as $key => $val)
+    {
+        $array_out[] = $array_in[$key];
+    }
+    // return the output array which is all nicely sorted by the index you wanted!
+    return $array_out;
+}
 
 function steamMeta()
 {
@@ -68,12 +81,11 @@ function steamData($steamId)
 	$resp = httpClient('https://store.steampowered.com/api/appdetails?appids='.$steamId.'&l=german', $cache);     // added trailing / to avoid redirect
     if (!$resp['success']) $CLIENTERROR .= $resp['error']."\n";
 	
-	$data['encoding'] = 'UTF-8';
-	
 	$jsonObject = json_decode($resp['data']);
 	
-	if(!$jsonObject->{$steamId}->{'success'}) $CLIENTERROR .= "Steam Id not found\n";
+	if(!($jsonObject->{$steamId}->{'success'})) {$CLIENTERROR .= "Steam Id not found\n";return $data;}
 	
+	$data['encoding'] = 'UTF-8';
 	$data['year'] = substr($jsonObject->{$steamId}->{'data'}->{'release_date'}->{'date'},-4);
 	$data['title'] = $jsonObject->{$steamId}->{'data'}->{'name'};
 	$coverurl = $jsonObject->{$steamId}->{'data'}->{'header_image'};
