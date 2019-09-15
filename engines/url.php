d4788ca851a72969f401c96bfa272581b1708d96
NF13 <6102845+NF13@users.noreply.github.com>
2019-09-15 19:18:26 +0200

    themoviedbtv and possible to add actors via url

---
+++ engines/url.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * IMDB Parser
+ *
+ * Parses data from the Internet Movie Database
+ *
+ * @package Engines
+ * @author  Andreas Gohr    <a.gohr@web.de>
+ * @link    http://www.imdb.com  Internet Movie Database
+ * @version $Id: imdb.php,v 1.76 2013/04/10 18:11:43 andig2 Exp $
+ */
+
+$GLOBALS['urlPrefix'] = 'url:';
+
+
+function urlActorUrl($name, $actorid)
+{
+
+    $ary = array();
+    $ary[0][0] = $name;
+    $ary[0][1] = $actorid;
+print_r($ary);
+    return $ary;
+}
+?>
