e9ea565321a0eefa4c5a66249a47d3a2bb3c8368
NF13 <6102845+NF13@users.noreply.github.com>
2019-09-22 10:37:54 +0200

    add shockchallenge

--- engines/url.php
+++ engines/url.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * IMDB Parser
- *
- * Parses data from the Internet Movie Database
- *
- * @package Engines
- * @author  Andreas Gohr    <a.gohr@web.de>
- * @link    http://www.imdb.com  Internet Movie Database
- * @version $Id: imdb.php,v 1.76 2013/04/10 18:11:43 andig2 Exp $
- */
 
 $GLOBALS['urlPrefix'] = 'url:';
 
